602c02b7cefb8cfbd7dd817e915b5f94e8057874
John Munster <56341207+johnmunster@users.noreply.github.com>
2020-04-15 10:28:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,23 +4,38 @@ const button = document.getElementById('random-btn');
 async function getFilmCollection() {
     const result = await fetch('./collection.json');
     const data = await result.json();
-    console.log(data);
+    //console.log(data);
 
+    // Random Button
+    button.addEventListener('click', function() {
+        // Randomise Films
+        let randomFilms = data.films[Math.floor(Math.random() * data.films.length)];
+        // Poster Image
+        const posterImage = document.getElementById('poster-img');
+        posterImage.src = randomFilms.posterImg;
+        // Film Title
+        const title = document.getElementById('film-title');
+        title.textContent = randomFilms.filmTitle;
+        // Release Date
+        const release = document.getElementById('release-date');
+        release.textContent = (`release date: ${randomFilms.releaseDate}`);
+        // Director
+        const director = document.getElementById('director-name');director.textContent = (`Directed by: ${randomFilms.director}`);
+        // Genre
+        const genre = document.getElementById('genre');genre.textContent = randomFilms.genre;
 
+        //console.log(posterImage);
 
-        button.addEventListener('click', function() {
-            let randomFilms = data.films[Math.floor(Math.random() * data.films.length)];
-            const posterImage = document.getElementById('poster-img');
-            posterImage
-            const title = document.getElementById('film-title');
-            title.textContent = randomFilms.filmTitle;
-            const release = document.getElementById('release-date');
-            release.textContent = (`release date: ${randomFilms.releaseDate}`);
-            const director = document.getElementById('director-name');director.textContent = (`Directed by: ${randomFilms.Director}`);
-            const genre = document.getElementById('genre');genre.textContent = randomFilms.Genre;
-        });
+    });
 
+    //data.films.forEach( film => console.log(film));
+
+    let fantasyFilms = data.films.filter(function (genres) {
+        return genres.certificate === '18'
+    });
+    console.log(fantasyFilms);
 
 };
 
+// Call function
 getFilmCollection();
